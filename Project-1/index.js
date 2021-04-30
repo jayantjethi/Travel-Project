51ed4bf276b83f0c5bef933536f78baf0cc7843f
Jindalh2 <jindalh2@gmail.com>
2021-04-30 15:55:27 +0530

    map button

--- Project-1/index.js
+++ Project-1/index.js
@@ -3,7 +3,7 @@ function updateMap() {
   fetch("/data.json")
     .then(response => response.json())
     .then(rsp => {
-      // console.log(rsp.data)
+       console.log(rsp.data)
       rsp.data.forEach(element => {
         latitude = element.latitude;
         longitude = element.longitude;
@@ -23,7 +23,7 @@ function updateMap() {
         })
           .setLngLat([longitude, latitude])
           .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-          .setHTML('<h3>' + element.name + '</h3><p>'))
+          .setHTML('<h3>' + element.name + '</h3><br><h3>'+ '<h3>Infected</h3>'+ element.infected + '</h3><br>'+'<h3>' +'<h3>Vaccinated</h3>'+ element.vaccinated + '</h3><br>'+'<h3>Recovered</h3>'+ element.recovered + '</h3><br>' ))
           .addTo(map);
       });
     })
