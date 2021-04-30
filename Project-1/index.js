677c0fccee5ef03f62bdd3e1cf6df88483382f79
jayantjethi <jayant.jethi8@gmail.com>
2021-04-30 14:22:04 +0530

    Changes

--- Project-1/index.js
+++ Project-1/index.js
@@ -3,7 +3,7 @@ function updateMap() {
   fetch("/data.json")
     .then(response => response.json())
     .then(rsp => {
-      // console.log(rsp.data)
+      console.log(rsp.data)
       rsp.data.forEach(element => {
         latitude = element.latitude;
         longitude = element.longitude;
