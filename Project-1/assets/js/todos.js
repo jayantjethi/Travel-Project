7a54a4c54e715d3a3dad3fc05fccc386522fc0b8
Jindalh2 <jindalh2@gmail.com>
2021-05-06 16:37:48 +0530

    Changes

--- Project-1/assets/js/todos.js
+++ Project-1/assets/js/todos.js
@@ -1,26 +1,120 @@
-// // Check Off Specific Todos By Clicking
-// $(".myul").on("click", "li", function(){
-//   $(this).toggleClass("completed");
-// });
+  $(".myul1").on("click", "li", function () {
+      $(this).toggleClass("completed");
+    });
 
-// //Click on X to delete Todo
-// $(".myul").on("click", "span", function(event){
-//   $(this).parent().fadeOut(500,function(){
-//     $(this).remove();
-//   });
-//   event.stopPropagation();
-// });
+    // when you click on dustbin, item gets removed from the todo list
+    $(".myul1").on("click", "span", function (event) {
+      $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+      });
 
-// $(".inputtodo").keypress(function(event){
-//   if(event.which === 13){
-//     //grabbing new todo text from input
-//     var todoText = $(this).val();
-//     $(this).val("");
-//     //create a new li and add to ul
-//     $(".myul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
-//   }
-// });
 
-// $(".fa-plus").click(function(){
-//   $(".inputtodo").fadeToggle();
-// });
+
+    });
+
+    // For entering a new item in to-do list
+    $(".inputtodo1").keypress(function (event) {
+      if (event.which === 13) {
+        var todo = $(this).val();
+        $(this).val("");
+        $(".myul1").append("<li><span>x </span>" + todo + "</li>");
+      }
+    });
+
+    $(".myul2").on("click", "li", function () {
+      $(this).toggleClass("completed");
+    });
+
+    // when you click on dustbin, item gets removed from the todo list
+    $(".myul2").on("click", "span", function (event) {
+      $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+      });
+
+
+
+    });
+
+    // For entering a new item in to-do list
+    $(".inputtodo2").keypress(function (event) {
+      if (event.which === 13) {
+        var todo = $(this).val();
+        $(this).val("");
+        $(".myul2").append("<li><span>x </span>" + todo + "</li>");
+      }
+    });
+
+    
+
+      $(".myul3").on("click", "li", function () {
+      $(this).toggleClass("completed");
+    });
+
+    // when you click on dustbin, item gets removed from the todo list
+    $(".myul3").on("click", "span", function (event) {
+      $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+      });
+
+
+
+    });
+
+    // For entering a new item in to-do list
+    $(".inputtodo3").keypress(function (event) {
+      if (event.which === 13) {
+        var todo = $(this).val();
+        $(this).val("");
+        $(".myul3").append("<li><span>x </span>" + todo + "</li>");
+      }
+    });
+
+     $(".myul4").on("click", "li", function () {
+      $(this).toggleClass("completed");
+    });
+
+    // when you click on dustbin, item gets removed from the todo list
+    $(".myul4").on("click", "span", function (event) {
+      $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+      });
+
+
+
+    });
+
+    // For entering a new item in to-do list
+    $(".inputtodo4").keypress(function (event) {
+      if (event.which === 13) {
+        var todo = $(this).val();
+        $(this).val("");
+        $(".myul").append("<li><span>x </span>" + todo + "</li>");
+      }
+    });
+
+    
+
+      $(".myul5").on("click", "li", function () {
+      $(this).toggleClass("completed");
+    });
+
+    // when you click on dustbin, item gets removed from the todo list
+    $(".myul5").on("click", "span", function (event) {
+      $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+      });
+
+
+
+    });
+
+    // For entering a new item in to-do list
+    $(".inputtodo5").keypress(function (event) {
+      if (event.which === 13) {
+        var todo = $(this).val();
+        $(this).val("");
+        $(".myul").append("<li><span>x </span>" + todo + "</li>");
+      }
+    });
+
+    
